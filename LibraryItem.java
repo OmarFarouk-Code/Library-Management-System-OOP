be3b834f6d6c1bb3ffd0c751daa9ed17784dd313
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-02-13 00:00:51 +0200

    Created the full library item class with Constructors aswell as setters/getters

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,6 +1,50 @@
+//PARENT CLASS
 public class LibraryItem 
 {
-    
+    //Private Variables
+    private String Id;
+    private String Title;
+    private boolean Available;
 
+    //Constructor
+    public LibraryItem(String Id , String Title)
+    {
+        this.Id = Id;
+        this.Title = Title;
+    }
 
+    //Getters and Setters
+    void getTitle ()
+    {
+        System.out.println("The Title is : " + Title);
+    }
+
+    void setTitle ( String InputtedTitle)
+    {
+        Title = InputtedTitle;
+    }
+
+    void getAvailabilty ()
+    {
+        if ( Available == true ) 
+        {
+            System.out.println("It's available");
+        }
+        else 
+        {
+            System.out.println("It's not available");
+        }
+    }
+
+    void setAvailabilty (boolean Availabilty)
+    {
+        Available = Availabilty;
+    }
+
+    void DisplayDetails ()
+    {
+        System.out.println("ID : " + Id);
+        getTitle();
+        getAvailabilty();
+    }
 }
