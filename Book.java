8c04b00da45f4caa0e4aad962e66e7cd402f494c
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-02-13 00:17:01 +0200

    Book Constructor and Override Method Added

--- Book.java
+++ Book.java
@@ -1,6 +1,24 @@
 public class Book extends LibraryItem 
 {
+    private String author;
+    private int pageCount;
+    private String ISBN;
+
+    public Book (String Id , String Title , String author ,int pageCount, String ISBN)
+    {
+        super(Id , Title);
+        this.author = author;
+        this.pageCount = pageCount;
+        this.ISBN = ISBN;
+    }
+
+    void DisplayDetails ()
+    {
+        super.DisplayDetails();
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pageCount);
+        System.out.println("ISBN: " + ISBN);  
+    }
 
 
-    
 }
