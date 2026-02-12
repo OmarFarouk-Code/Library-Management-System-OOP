f93fd915fa221b47d122264921bbac143ef621eb
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-02-13 00:33:18 +0200

    Created User Parent Class and Initalized user basic info

---
+++ User.java
@@ -0,0 +1,12 @@
+//PARENT CLASS
+public class User 
+{
+    private String name;
+    private String UserID;
+    
+    public User (String name , String UserID)
+    {
+        this.name = name;
+        this.UserID = UserID;
+    }
+}
