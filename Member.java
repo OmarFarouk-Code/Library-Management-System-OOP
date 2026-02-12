b54bbfc8cb73a6188dc491006fe4c441a7505817
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-02-13 00:51:57 +0200

    Added New Methods for Library Members

---
+++ Member.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Member extends User
+{
+   private List<LibraryItem> borrowedItems; 
+
+   public Member (String name , String UserID)
+   {
+        super( name , UserID ); 
+   }
+
+   public void borrowItem (LibraryItem Item)
+   {
+        borrowedItems.add(Item);
+        System.out.print(" Borrowed: ");
+        Item.getTitle();
+   }
+
+   public void returnItem (String id)
+   {
+        for (int i = 0 ; i < borrowedItems.size() ; i++ )
+        {
+            if ( borrowedItems.get(i).getID() == id )
+            {
+                borrowedItems.remove(i);
+                System.out.println("Item with ID " + id + " has been removed.");
+                return;
+            }
+        }
+        System.out.println("Item with Entered ID was not found");
+   }
+}
