c17cfa9873c5f295592ee5fc100daabf2ef987bb
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-02-13 01:02:49 +0200

    Created Librarian Class , Initalized its constructor , created methods for adding and removing items to inventory

---
+++ Librarian.java
@@ -0,0 +1,39 @@
+public class Librarian extends User
+{
+    private String employeeID;
+
+    public Librarian ( String name , String UserID )
+    {
+        super( name , UserID);
+    }
+
+    void addItem(LibraryItem Item)
+    {
+        inventory.add(Item);
+        System.out.print("New item added to inventory: ");
+        Item.getTitle();
+    }
+
+    void removeItem(String ID)
+    {
+        boolean found = false;
+        for (int i = 0 ; i < inventory.size() ; i++)
+        {
+            if (inventory.get(i).getID() ==  ID)
+            {
+                inventory.remove(i);
+                System.out.println("Item " + ID + " has been deleted from the system.");
+                found = true;
+            break;
+            }
+        }
+        if (found == false)
+        {
+            System.out.println("Error: Item " + ID + " not found in inventory.");
+        }
+    }
+
+
+
+
+}
